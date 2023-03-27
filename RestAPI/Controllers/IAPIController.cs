7589fb7918c38f522272083725d44e056c96bad5
darkfrankdl <128466282+darkfrankdl@users.noreply.github.com>
2023-03-27 07:33:50 +0200

    Add files via upload

--- RestAPI/Controllers/IAPIController.cs
+++ RestAPI/Controllers/IAPIController.cs
@@ -5,11 +5,9 @@ namespace RestAPI.Controllers
 {
     public interface IAPIController
     {
-        //ActionResult<int> AddTopic(Topic topic);
 
         ActionResult AddTopic(Topic topic);
         ActionResult<IEnumerable<Topic>> GetTopic();
-        //ActionResult<bool> UpdateTopic(Topic topic);
         public ActionResult UpdateTopic(Topic topic);
         public ActionResult DeleteTopic(Topic topic);
 
